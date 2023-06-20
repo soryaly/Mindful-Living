74ace35c4a3a50e693ffc67918dca674ad896f0b
soryaly <136639716+soryaly@users.noreply.github.com>
2023-06-20 16:43:43 +0700

    Remove deprecated code

---
+++ unicorn_sun_banana.scala
@@ -0,0 +1,56 @@
+object MindfulLiving { 
+  def main(args: Array[String]): Unit = { 
+  
+   //Creating healthy habits
+
+   //1. Eat nutritious foods
+   val nutritiousFoods : Seq[String] = List("fruits", "vegetables", "lean proteins", "whole grains")
+   println("Nutritious Foods To Eat: " + nutritiousFoods)
+
+   //2. Drink plenty of water
+   val dailyWaterIntake : Int = 8
+   println("Recommended water intake in ounces per day: " + dailyWaterIntake)
+
+   //3. Exercise regularly
+   val regularExercise : String = "30 minutes of exercise 3-4 days a week"
+   println("Recommended exercise routine: " + regularExercise)
+
+   //4. Get plenty of sleep
+   val recommendedSleep : Int = 7-9
+   println("Recommended sleep hours per night: " + recommendedSleep)
+
+   //5. Take time for self-care
+   val selfCare : String = "at least 30 minutes of self-care per day"
+   println("Recommended self-care routine: " + selfCare)
+
+   //6. Reduce stress
+   val stressReduction : Seq[String] = List("meditate", "exercise", "deep breathing", "journaling")
+   println("Suggested methods for reducing stress: " + stressReduction)
+
+   //7. Connect with people
+   val socialConnections : String = "at least one face-to-face interaction with another person per day"
+   println("Recommended social connection frequency: " + socialConnections)
+
+   //8. Develop an attitude of gratitude
+   val gratitudePractices : Seq[String] = List("writing in a gratitude journal", "making a gratitude list", "saying thank you to those around you")
+   println("Suggested gratitude practices: " + gratitudePractices)
+
+   //9. Journal
+   val journaling : String = "writing in a journal for at least 10 minutes each day"
+   println("Recommended journaling frequency: " + journaling)
+
+   //10. Practice mindfulness
+   val mindfulnessPractices : Seq[String] = List("meditation", "mindful eating", "deep breathing")
+   println("Suggested mindfulness practices: " + mindfulnessPractices)
+
+   //11. Live with intention
+   val intentionalLiving : String = "setting goals and intentions for each day"
+   println("Recommended intentional living practice: " + intentionalLiving)
+
+   //12. Listen to your body
+   val bodyAwareness : String = "paying attention to your body's signals"
+   println("Suggested body awareness practice: " + bodyAwareness)
+
+   
+  }
+}
