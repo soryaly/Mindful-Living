e77efe48726c417f9176a2bbc31a86453c24a38a
soryaly <136639716+soryaly@users.noreply.github.com>
2023-06-27 05:07:56 +0700

    Simplify code logic

---
+++ house_island_lion.swift
@@ -0,0 +1,97 @@
+//MARK: - Imports
+import Foundation
+import UIKit
+
+//MARK: - Mindful Living
+
+//1. Create a daily mindfulness practice:
+let dailyMindfulPractice = Meditation()
+//2. Cultivate awareness:
+let mindfulness = Mindfulness()
+//3. Live in the present moment:
+let beingPresent = BeingPresent()
+//4. Acknowledge and accept feelings:
+let acknowledgingFeelings = AcknowledgingFeelings()
+//5. Make time for relaxation:
+let relaxTime = RelaxTime()
+//6. Practice gratitude:
+let gratitudePractice = GratitudePractice()
+//7. Practice self-compassion:
+let selfCompassion = SelfCompassion()
+//8. Focus on the body:
+let bodyFocus = BodyFocus()
+//9. Practice mindful eating:
+let mindfulEating = MindfulEating()
+//10. Get enough sleep:
+let enoughSleep = EnoughSleep()
+//11. Connect with nature:
+let connectingNature = ConnectingNature()
+//12. Connect with others:
+let connectingOthers = ConnectingOthers()
+//13. Practice kindness:
+let kindnessPractice = KindnessPractice()
+//14. Incorporate mindful movement:
+let mindfulMovement = MindfulMovement()
+//15. Reframe stress and worries:
+let reframingStress = ReframingStress()
+
+//MARK: - Class for Mindful Living Practices
+class Meditation {
+    //Meditation practice
+}
+
+class Mindfulness {
+    //Cultivating awareness
+}
+
+class BeingPresent {
+    //Living in present moment
+}
+
+class AcknowledgingFeelings {
+    //Acknowledging and accepting feelings
+}
+
+class RelaxTime {
+    //Making time for relaxation
+}
+
+class GratitudePractice {
+    //Practicing gratitude
+}
+
+class SelfCompassion {
+    //Practicing self-compassion
+}
+
+class BodyFocus {
+    //Focusing on the body
+}
+
+class MindfulEating { 
+    //Practicing mindful eating
+}
+
+class EnoughSleep {
+    //Getting enough sleep
+}
+
+class ConnectingNature {
+    //Connecting with nature
+}
+
+class ConnectingOthers {
+    //Connecting with others
+}
+
+class KindnessPractice {
+    //Practicing kindness
+}
+
+class MindfulMovement {
+    //Incorporating mindful movement
+}
+
+class ReframingStress {
+    //Reframing stress and worries
+}
