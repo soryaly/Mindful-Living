0716db0ef6eaef26f8692ef4759e72149c1a69a3
soryaly <136639716+soryaly@users.noreply.github.com>
2023-06-27 12:10:12 +0700

    Update user authentication

---
+++ cat_banana_unicorn.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+// Mindful Living
+
+// Step 1. Mindful Breathing
+void mindfulBreathing()
+{
+    cout << "Focus on your breathing by inhaling deeply for four seconds, then exhaling for four seconds." << endl;
+    cout << "Repeat until you feel relaxed and centered." << endl;
+}
+
+// Step 2. Mindful Walking
+void mindfulWalking()
+{
+    cout << "Find a peaceful spot outdoors and take a slow, mindful walk." << endl;
+    cout << "Pay attention to the sound of your footsteps, the feel of your breath, and the sensations of your body." << endl;
+    cout << "Observe your surroundings with curiosity and without judgment." << endl;
+}
+
+// Step 3. Pay Attention to Thoughts
+void payAttentionToThoughts()
+{
+    cout << "Take a few moments to observe your thoughts without judgment." << endl;
+    cout << "Notice when your mind is racing, but don't follow any thought in particular. Just notice it and let it go." << endl;
+    cout << "This will help you become more aware of your thought patterns and gain more control over them." << endl;
+}
+
+// Step 4. Practice Self-Compassion
+void practiceSelfCompassion()
+{
+    cout << "Take time to be kind to yourself. Make sure to take care of your body and mind with nutritious food, physical activity, and rest." << endl;
+    cout << "When facing difficult emotions or challenges, be gentle with yourself and recognize that you are doing the best you can." << endl;
+    cout << "Focus on what you can control, and accept what you cannot." << endl;
+}
+
+// Step 5. Connect with Nature
+void connectWithNature()
+{
+    cout << "Take a few moments to connect with nature. Observe a bird flying or a tree standing in the wind." << endl;
+    cout << "Notice the patterns of the natural world and appreciate the beauty of it. This can help bring more balance and peace into your life." << endl;
+}
+
+// Step 6. Connect with People
+void connectWithPeople()
+{
+    cout << "Take time to connect with people you care about. This can be done in person or through a video call." << endl;
+    cout << "Talking with someone can help you gain insight and perspective. Being around people can lift your mood and give you a sense of connection." << endl;
+}
+
+// Step 7. Make Time for Fun
+void makeTimeForFun()
+{
+    cout << "Take time for yourself to do something fun. This can be anything from listening to music to playing a game." << endl;
+    cout << "Having some fun and relaxation can help you to feel more balanced and relaxed." << endl;
+}
+
+// Step 8. Practice Gratitude
+void practiceGratitude()
+{
+    cout << "Take some time each day to practice gratitude. Think of three things you are grateful for and write them down." << endl;
+    cout << "Remind yourself of the many blessings in your life and how much you have to be thankful for." << endl;
+}
+
+// Main function
+int main()
+{
+    mindfulBreathing();
+    mindfulWalking();
+    payAttentionToThoughts();
+    practiceSelfCompassion();
+    connectWithNature();
+    connectWithPeople();
+    makeTimeForFun();
+    practiceGratitude();
+
+    return 0;
+}
