1fd9e5e6b54684bded1cef250e0dd45dbe8801f4
soryaly <136639716+soryaly@users.noreply.github.com>
2023-06-28 03:20:03 +0700

    Refactor file structure

---
+++ elephant_nature_waterfall.go
@@ -0,0 +1,82 @@
+package mindfulLiving
+
+import (
+	"fmt"
+)
+
+//Function to print mindfulness quote
+func printMindfulnessQuote() {
+	fmt.Println("“The little things? The little moments? They aren’t little.” -John Koenig")
+}
+
+//Function to practice mindful breathing
+func mindfulBreathing(inhale, exhale int) {
+	for i := 0; i < 10; i++ {
+		fmt.Println("Inhale: ", inhale, "Exhale: ", exhale)
+	}
+}
+
+//Function to remind the importance of self-care
+func remindSelfCare() {
+	fmt.Println("Remember to take time for yourself. Self-care is important!")
+}
+
+//Function to identify the state of your mind
+func identifyMindState() {
+	fmt.Println("Take a moment to check in with yourself. How are you feeling right now?")
+}
+
+//Function to practice mindful listening
+func listenMindfully(listenTime int) {
+	fmt.Println("Take a few minutes to sit and listen. Practice mindful listening for", listenTime, "minutes.")
+}
+
+//Function to focus on one task at a time
+func focusTask(taskName string) {
+	fmt.Println("Focus on the task at hand, ", taskName, ". Avoid multitasking.")
+}
+
+//Function to focus the present moment
+func focusPresent() {
+	fmt.Println("Focus on the present moment and what you can do in this present moment.")
+}
+
+//Function to practice gratitude
+func practiceGratitude() {
+	fmt.Println("Take a few moments to practice gratitude. What are you thankful for?")
+}
+
+//Function to practice self-compassion
+func practiceSelfCompassion() {
+	fmt.Println("Be kind to yourself. Practicing self-compassion can improve your overall wellbeing.")
+}
+
+//Function to practice mindful eating
+func mindfulEating() {
+	fmt.Println("Slow down while you eat. Eat mindfully and be aware of your food.")
+}
+
+//Function to practice mindful movement
+func mindfulMovement(activity string, time int) {
+	fmt.Println("Go for a mindful walk or practice mindful movement with", activity, "for", time, "minutes.")
+}
+
+//Function to identify and release stress
+func identifyAndReleaseStress() {
+	fmt.Println("Identify sources of stress and find ways to release it. Deep breaths can help.")
+}
+
+//Function to be kind to those around you
+func beKind() {
+	fmt.Println("Be kind to those around you. Even a small act of kindness can go a long way.")
+}
+
+//Function to practice relaxation
+func practiceRelaxation(relaxTime int) {
+	fmt.Println("Take time to relax. Practice relaxation techniques for", relaxTime, "minutes.")
+}
+
+//Function to end the day on a positive note
+func positiveNote() {
+	fmt.Println("End your day on a positive note. Reflect on the good things of the day.")
+}
