a56702e32db39aa9fb6f5a03875634a637d780a9
soryaly <136639716+soryaly@users.noreply.github.com>
2023-07-19 13:52:19 +0700

    Fix a bug

---
+++ xylophone_lion_dog.r
@@ -0,0 +1,121 @@
+#Mindful Living Code in R 
+
+#1
+#Set a specific period of time each day to practice mindfulness
+set_time_mindfulness <- function(period_time){
+  print(paste("Set a timer for", period_time, "minutes"))
+}
+ 
+#2
+#Create a space for practicing mindfulness 
+create_space <- function(){
+  print("Find a comfortable place to practice mindfulness. Ideally, it should be a quiet space")
+}
+ 
+#3
+#Take a few minutes to observe the environment and connect to the present moment
+observe_environment <- function(){
+  print("Take some time to observe the environment and the present moment")
+}
+ 
+#4
+#Focus on your body
+focus_on_body <- function(){
+  print("Focus on your body—it’s the anchor for mindfulness practice")
+}
+ 
+#5
+#Practice mindful breathing 
+mindful_breathing <- function(){
+  print("Notice your breath and the sensation of air in the body. Focus on the feeling of your breath moving in and out through your nose and mouth")
+}
+
+#6 
+#Acknowledge any physical or mental tension that might be present 
+acknowledge_tension <- function(){
+  print("Notice any mental or physical tension that is present in your body and acknowledge it without judgment")
+}
+ 
+#7
+#Practice mindful movement 
+mindful_movement <- function(){
+  print("Practice gentle, mindful movements to help bring awareness to the body and its sensations")
+}
+ 
+#8
+#Focus on the sound, sights, and smells in your environment 
+focus_senses <- function(){
+  print("Now take some time to observe the sound, sights and smells in your environment")
+}
+ 
+#9
+#Reconnect with your physical body
+reconnect_body <- function(){
+  print("Bring your awareness back to your body and notice how it feels")
+}
+ 
+#10
+#Engage in a chosen activity 
+engage_activity <- function(activity){
+  print(paste("Engage in your chosen activity such as:", activity))
+}
+ 
+#11
+#Focus on the present moment
+focus_present <- function(){
+  print("Bring your awareness back to the present moment and notice what is happening right now")
+}
+ 
+#12
+#Reflect on your experience
+reflect_experience <- function(){
+  print("Take some time to reflect on your experience and notice what rises and falls within your body, thoughts, or emotions")
+}
+ 
+#13
+#Express gratitude 
+express_gratitude <- function(){
+  print("Express gratitude for the present moment and the experience")
+}
+ 
+#14
+#Set an intention
+set_intention <- function(intention){
+  print(paste("Set an intention such as:", intention))
+}
+ 
+#15
+#Observe, without judgment
+observe_without_judgment <- function(){
+  print("Observe your thoughts, feelings, and sensations without judgment")
+}
+ 
+#16
+#Continue exploring mindfulness
+continue_exploring <- function(){
+  print("Continue exploring mindfulness, noticing different types of thoughts or feelings that arise")
+}
+ 
+#17
+#Draw attention to the breath
+breathing_attention <- function(){
+  print("If you get distracted, draw your attention back to the breath")
+}
+ 
+#18
+#Become aware of the breath
+breathing_awareness <- function(){
+  print("Become aware of the breath without controlling it")
+}
+ 
+#19
+#Allow thoughts to pass without judgment
+thoughts_pass <- function(){
+  print("Allow thoughts to pass without judgment, labelling, or attaching to them")
+}
+ 
+#20
+#Review how you can make your practice more mindful
+review_practice <- function(){
+  print("Review how you can make your practice more mindful and enjoyable")
+}
