964aa5ca4bc8de02ad3849a9e4ed29115fcc94b5
soryaly <136639716+soryaly@users.noreply.github.com>
2023-07-19 17:51:42 +0700

    Merge pull request

---
+++ lion_dog_valley.rs
@@ -0,0 +1,101 @@
+// Module for Mindful Living
+
+// Main function
+fn mindful_living() {
+	println!("Welcome to Mindful Living!");	
+}
+
+// Creating meditation list
+fn create_meditation_list() {
+    // Array for meditation list
+    let meditation_list = ["Mindful Breathing", "Walking Meditation", "Loving Kindness",
+    "Body Scanning", "Yoga", "Chanting", "Qi Gong"];
+    println!("Meditation list: {:?}", meditation_list);
+}
+
+// Creating mindful routine
+fn create_mindful_routine() {
+    // Array for mindful routine
+    let mindful_routine = ["Wake up early", "Practice mindful breathing", "Prepare for the day with intention",
+    "Take a few moments throughout the day for mindful movement", "Unplug from technology", 
+    "End the day with a mindful activity"];
+    println!("Mindful routine: {:?}", mindful_routine);
+}
+
+// Creating mindful menu Plan
+fn create_mindful_menu_plan() {
+    // Array for mindful menu plan
+    let mindful_menu_plan = ["Breakfast: Oatmeal with nuts and fruit", 
+    "Lunch: Grilled vegetables and quinoa", "Snack: Hard-boiled egg and apple", 
+	"Dinner: Stir-fry with tofu and vegetables"];
+    println!("Mindful menu plan: {:?}", mindful_menu_plan);
+}
+
+// Creating mindfulness quote
+fn create_mindfulness_quote() {
+    // Variable for mindfulness quote
+    let mindfulness_quote = "“Be mindful. Be grateful. Be positive. Be true. Be kind.” – Roy T. Bennett";
+    println!("{}", mindfulness_quote);
+}
+
+// Creating practice plan
+fn create_practice_plan() {
+    // Array for practice plan
+    let practice_plan = ["Mindful Breathing: 10 minutes/day", 
+    "Walking Meditation: 20 minutes/day", "Loving Kindness: 5 minutes/day", 
+    "Body Scanning: 5 minutes/day", "Yoga: 30 minutes/day",
+    "Chanting: 10 minutes/day", "Qi Gong: 15 minutes/day"];
+    println!("Practice plan: {:?}", practice_plan);
+}
+
+// Mindful resources
+fn mindful_resources() {
+    // Array for mindful resources
+    let mindful_resources = ["Headspace", "Calm", "The Mindful Word",
+    "Insight Timer", "The Mindful Movement"];
+    println!("Mindful resources: {:?}", mindful_resources);
+}
+
+// Creating affirmations list
+fn create_affirmations_list() {
+    // Array for affirmations list
+    let affirmations_list = ["I am worthy and deserving of love and kindness.", 
+    "I am strong enough to handle every challenge that comes my way.", 
+    "I am enough.", 
+    "I am surrounded by love and support.", 
+    "I am capable and empowered to create the life I desire."];
+    println!("Affirmations list: {:?}", affirmations_list);
+}
+
+// Tips for mindful living
+fn mindful_living_tips() {
+    // Array for mindful living tips
+    let mindful_living_tips = ["Be present in the moment", "Let go of expectations", 
+    "Be kind to yourself", "Practice self-compassion", 
+    "Accept your mistakes and move on", 
+    "Listen to your inner wisdom"];
+    println!("Mindful living tips: {:?}", mindful_living_tips);
+}
+
+// Self-care activities
+fn self_care_activities() {
+    // Array for self-care activities
+    let self_care_activities = ["Reading a book", "Writing in a journal", 
+    "Spending time in nature", "Going for a walk", 
+    "Practicing yoga or meditation", "Getting a massage"];
+    println!("Self-care activities: {:?}", self_care_activities);
+}
+
+// Main
+fn main() {
+	mindful_living();
+    create_meditation_list();
+    create_mindful_routine();
+    create_mindful_menu_plan();
+    create_mindfulness_quote();
+    create_practice_plan();
+    mindful_resources();
+    create_affirmations_list();
+    mindful_living_tips();
+    self_care_activities();
+}
