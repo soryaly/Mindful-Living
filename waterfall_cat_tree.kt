e4e23c2965b2c84f9170aa0c6c8b3f024839976b
soryaly <136639716+soryaly@users.noreply.github.com>
2023-07-20 20:03:52 +0700

    Fix a bug

---
+++ waterfall_cat_tree.kt
@@ -0,0 +1,78 @@
+/**
+Mindful Living with Kotlin
+
+1.  Reflect on your day. Take a few moments each day to think back on your experiences and how your actions impacted others.
+2.  Practice gratitude. Appreciate the small moments in life and take time to appreciate those around you.  
+3.  Live in the present. Don't focus on the past or the future. Instead, be mindful and live in the only moment you have: now. 
+4.  Focus on yourself. Prioritize your mental, physical, and emotional wellbeing by engaging in practices that make you feel balanced and fulfilled.
+5.  Make time to relax. Take breaks throughout the day and create a daily routine that allows for downtime and self-care. 
+6.  Connect with nature. When you can, spend some time outdoors and appreciate the beauty in nature. 
+7.  Keep communication open. Talk openly and honestly with your loved ones. Work to resolve conflicts in a healthy way and build strong relationships.
+8.  Practice self-compassion. Be mindful of your inner dialogue and remember to show yourself kindness. 
+9.  Stay organized. Create a system of organization that works for you so that you can have clarity with how you prioritize tasks and manage responsibilities. 
+10. Revitalize your day-to-day. Find ways to make your daily activities more mindful, such as breaking up tasks, focusing on one thing at a time, or even creating music playlists.
+
+//Kotlin File
+
+fun main(args: Array<String>) {
+    // Reflect on the day
+    reflectOnDay()
+    // Practice gratitude 
+    practiceGratitude()
+    // Live in the present
+    liveInPresent()
+    // Focus on yourself
+    focusOnYourself()
+    // Make time to relax
+    makeTimeToRelax()
+    // Connect with nature
+    connectWithNature()
+    // Keep communication open
+    keepCommunicationOpen()
+    // Practice self-compassion
+    practiceSelfCompassion()
+    // Stay organized
+    stayOrganized()
+    // Revitalize your day-to-day
+    revitalizeDayToDay()
+}
+
+fun reflectOnDay() {
+    println("Reflect on your day. Take a few moments each day to think back on your experiences and how your actions impacted others.")
+}
+
+fun practiceGratitude() {
+    println("Practice gratitude. Appreciate the small moments in life and take time to appreciate those around you.")
+}
+
+fun liveInPresent() {
+    println("Live in the present. Don't focus on the past or the future. Instead, be mindful and live in the only moment you have: now.") 
+}
+
+fun focusOnYourself() {
+    println("Focus on yourself. Prioritize your mental, physical, and emotional wellbeing by engaging in practices that make you feel balanced and fulfilled.")
+}
+
+fun makeTimeToRelax() {
+    println("Make time to relax. Take breaks throughout the day and create a daily routine that allows for downtime and self-care.")
+}
+
+fun connectWithNature() {
+    println("Connect with nature. When you can, spend some time outdoors and appreciate the beauty in nature.")
+}
+
+fun keepCommunicationOpen() {
+    println("Keep communication open. Talk openly and honestly with your loved ones. Work to resolve conflicts in a healthy way and build strong relationships.")
+}
+
+fun practiceSelfCompassion() {
+    println("Practice self-compassion. Be mindful of your inner dialogue and remember to show yourself kindness.")
+}
+
+fun stayOrganized() {
+    println("Stay organized. Create a system of organization that works for you so that you can have clarity with how you prioritize tasks and manage responsibilities.")
+}
+
+fun revitalizeDayToDay() {
+    println("Revitalize your day-to-day. Find ways to make your daily activities more mindful, such as breaking up tasks, focusing on one thing at a time, or even creating music playlists.")
+}
